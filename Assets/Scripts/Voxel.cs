90fee45ad0b73a89d78bed97689cac571cb7fee6
Vaynonym <timruschke@googlemail.com>
2021-07-24 14:35:57 +0200

    Add position to voxel

--- Assets/Scripts/Voxel.cs
+++ Assets/Scripts/Voxel.cs
@@ -1,8 +1,11 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Voxel : MonoBehaviour {
 	private new Rigidbody rigidbody;
+
+	public int3 position;
 
 	private void Awake() {
 		rigidbody = GetComponent<Rigidbody>();
