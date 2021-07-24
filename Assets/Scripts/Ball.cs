df8315ba1ef8e1c663c96778b84a06837bd06909
Sebastian Paarmann <sebastian@s-paarmann.de>
2021-07-24 16:28:18 +0200

    Add controller for shooting some balls at the voxels 🎳

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Ball : MonoBehaviour {
+	public bool keepVelocityAfterCollision = false;
+
+	private new Rigidbody rigidbody;
+	private Vector3 lastVelocity;
+
+	private void Awake() {
+		rigidbody = GetComponent<Rigidbody>();
+	}
+
+	private void FixedUpdate() {
+		lastVelocity = rigidbody.velocity;
+	}
+
+	private void OnCollisionEnter(Collision col) {
+		if (keepVelocityAfterCollision) {
+			rigidbody.AddForce(-col.impulse, ForceMode.Impulse);
+			//rigidbody.velocity = lastVelocity;
+		}
+	}
+
+	private void OnCollisionStay(Collision col) {
+		if (keepVelocityAfterCollision) {
+			rigidbody.AddForce(-col.impulse, ForceMode.Impulse);
+		}
+	}
+}
---
+++ Assets/Scripts/ShootBallsController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ShootBallsController : MonoBehaviour {
+	public Rigidbody ballPrefab;
+	public float ballSpeed;
+
+	private void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			Rigidbody ball = Instantiate(ballPrefab, ray.origin, Quaternion.identity);
+			ball.velocity = ray.direction * ballSpeed;
+		}
+	}
+}
